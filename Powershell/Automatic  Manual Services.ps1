4e8f4c70414a43df9ccb9946a8276e88a6a3be13
oznur-ceng <74248391+oznur-ceng@users.noreply.github.com>
2021-06-26 12:12:41 +0300

    Add files via upload

---
+++ Powershell/Automatic  Manual Services.ps1
@@ -0,0 +1,4 @@
+﻿Get-Service | Select-Object -Property Name,Status,StartType | Where-Object {$_.StartType -eq "Automatic"}
+
+
+Get-Service | Select-Object -Property Name,Status,StartType | Where-Object {$_.StartType -eq "Manual"}
---
+++ Powershell/PostMethodApi.ps1
@@ -0,0 +1,9 @@
+﻿$Cred = Get-Credential
+$Url = "https://server.contoso.com:8089/services/search/jobs/export"
+$Body = @{
+    search = "search index=_internal | reverse | table index,host,source,sourcetype,_raw"
+    output_mode = "csv"
+    earliest_time = "-2d@d"
+    latest_time = "-1d@d"
+}
+Invoke-RestMethod -Method 'Post' -Uri $url -Credential $Cred -Body $body -OutFile output.csv
